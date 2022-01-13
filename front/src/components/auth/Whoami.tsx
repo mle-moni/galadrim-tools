1cb8ea643025b618ef5b56a26d8393ee81a1311d
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2022-01-13 22:56:20 +0100

    feat: added front to create apiToken

--- front/src/components/auth/Whoami.tsx
+++ front/src/components/auth/Whoami.tsx
@@ -1,13 +1,17 @@
 import { Button } from '@mui/material'
 import { AppStore } from '../../stores/AppStore'
+import { WhoamiStore } from './WhoamiStore'
 
 export const Whoami = () => {
     const { authStore } = AppStore
+    const store = new WhoamiStore()
 
     return (
         <>
             <Button onClick={() => authStore.logout()}>Se déconnecter</Button>
-            <p style={{ textAlign: 'center' }}>{authStore.user.username}</p>
+            <p onClick={() => store.onClick()} style={{ textAlign: 'center', userSelect: 'none' }}>
+                {authStore.user.username}
+            </p>
         </>
     )
 }
