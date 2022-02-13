ce1817b9cf4cbaa7362be35f378a554ee64ea3f6
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2022-02-13 14:29:30 +0100

    fix: deleted useless block

--- front/src/pages/room/index.tsx
+++ front/src/pages/room/index.tsx
@@ -1,7 +1,6 @@
 import { Button } from '@mui/material'
 import { useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import { Whoami } from '../../components/auth/Whoami'
+import { useParams } from 'react-router-dom'
 import { RoomCalendar } from '../../components/Calendar'
 import MainLayout from '../../layouts/MainLayout'
 import { AppStore } from '../../stores/AppStore'
@@ -15,13 +14,8 @@ const RoomPage = () => {
     return (
         <MainLayout>
             <div>
-                <div style={{ position: 'absolute', right: '10px', top: '10px', zIndex: 10 }}>
-                    {AppStore.authStore.connected ? <Whoami /> : <></>}
-                </div>
                 <div style={{ position: 'absolute', left: '10px', top: '10px', zIndex: 10 }}>
-                    <Button>
-                        <Link to="/">Retour</Link>
-                    </Button>
+                    <Button onClick={() => AppStore.navigate('/')}>Retour</Button>
                 </div>
                 <RoomCalendar />
             </div>
