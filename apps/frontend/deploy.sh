4954d4e65f73f900b43fca12267ed5918eae21b0
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2022-05-04 00:23:04 +0200

    feat: improved deployment scripts

--- apps/frontend/deploy.sh
+++ apps/frontend/deploy.sh
@@ -4,5 +4,5 @@ BUILD_PATH="../../dist/apps/frontend"
 DEPLOY_PATH="/var/www/galadrim-rooms"
 
 stat $BUILD_PATH &> /dev/null  || (echo "frontend should be built before" && exit 1) &&
-rm -rf $DEPLOY_PATH || echo -n &&
-cp -r $BUILD_PATH $DEPLOY_PATH
+sudo rm -rf $DEPLOY_PATH || echo -n &&
+sudo cp -r $BUILD_PATH $DEPLOY_PATH
--- deployAll.sh
+++ deployAll.sh
@@ -2,12 +2,16 @@
 
 PM2_NAME="galadrim-rooms"
 
-git pull &&
 yarn install &&
+nx test backend &&
+nx test shared &&
 nx build frontend &&
-nx build backend &&
+TIME_DOWN=`date +%s`
 (pm2 show $PM2_NAME | grep online && pm2 stop $PM2_NAME || echo -n) &&
+nx build backend &&
 nx migrate-prod backend &&
 nx deploy backend &&
 nx deploy frontend &&
+TIME_UP=`date +%s`
 echo "Deployment success"
+echo "down time -->" `expr $TIME_UP - $TIME_DOWN` seconds
