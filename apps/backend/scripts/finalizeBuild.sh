d25c03b753d74e433a8f37fc64b5fd80cc687731
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2022-05-03 20:04:09 +0200

    feat: temporary node_modules hack

--- apps/backend/scripts/finalizeBuild.sh
+++ apps/backend/scripts/finalizeBuild.sh
@@ -2,7 +2,13 @@
 
 BUILD_PATH="../../dist/apps/backend"
 
+
+stat $BUILD_PATH/../../libs/shared &> /dev/null  || (echo "shared library should be built before" && exit 1)
 mv $BUILD_PATH/apps/backend/* $BUILD_PATH &&
 rmdir $BUILD_PATH/apps/backend &&
 rmdir $BUILD_PATH/apps &&
-cp .env $BUILD_PATH
+cp .env $BUILD_PATH &&
+cd $BUILD_PATH &&
+mkdir -p node_modules/@galadrim-rooms &&
+cd node_modules/@galadrim-rooms &&
+ln -s ../../../../libs/shared shared
