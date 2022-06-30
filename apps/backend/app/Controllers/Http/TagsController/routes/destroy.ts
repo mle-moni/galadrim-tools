a4e6e5ce649df71df780f78fd3c31651bf42133c
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2022-06-30 15:18:11 +0200

    feat: tags api

--- apps/backend/app/Controllers/Http/TagsController/routes/destroy.ts
+++ apps/backend/app/Controllers/Http/TagsController/routes/destroy.ts
@@ -1,16 +1,16 @@
 import { hasRights } from '@galadrim-rooms/shared/'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Restaurant from '../../../../Models/Restaurant'
+import Tag from '../../../../Models/Tag'
 import Ws from '../../../../Services/Ws'
 
 export const destroyRoute = async ({ params, auth, response }: HttpContextContract) => {
-    const restaurant = await Restaurant.findOrFail(params.id)
+    const tag = await Tag.findOrFail(params.id)
     const user = auth.user!
     if (!hasRights(user.rights, ['MIAM_ADMIN'])) {
         return response.forbidden({ error: `Vous n'avez pas les droits nécessaires` })
     }
-    const eventJson = restaurant.toJSON()
-    await restaurant.delete()
+    const eventJson = tag.toJSON()
+    await tag.delete()
     Ws.io.to('connectedSockets').emit('deleteTag', eventJson)
-    return { id: restaurant.id, deleted: true }
+    return { id: tag.id, deleted: true }
 }
