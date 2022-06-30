1056bc8d6f7b0162dd31d67d0b2534afc2747181
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2022-06-30 22:07:59 +0200

    feat: load tags in RestaurantsController

--- apps/backend/app/Controllers/Http/RestaurantsController/routes/index.ts
+++ apps/backend/app/Controllers/Http/RestaurantsController/routes/index.ts
@@ -2,5 +2,5 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Restaurant from '../../../../Models/Restaurant'
 
 export const indexRoute = async (_params: HttpContextContract) => {
-    return Restaurant.all()
+    return Restaurant.query().preload('tags')
 }
