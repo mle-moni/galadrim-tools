4d53b9214aec7a75ab5a15b14b2e99450fc629d7
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2022-07-12 00:51:41 +0200

    fix: prettier

--- apps/frontend/src/components/Core/Card.tsx
+++ apps/frontend/src/components/Core/Card.tsx
@@ -1,8 +1,8 @@
 import {
     Card as MuiCard,
     CardProps as MuiCardProps,
-    styled,
     experimental_sx as sx,
+    styled,
 } from '@mui/material'
 
 interface CardProps extends MuiCardProps {
@@ -13,13 +13,13 @@ const Root = styled(MuiCard)<CardProps>(({ size, theme }) =>
     sx({
         p: size === 'normal' ? 4 : 8,
         borderRadius: 1.5,
-          boxShadow: `0 5px 10px 0 rgba(31, 51, 86, 0.06)`,
+        boxShadow: `0 5px 10px 0 rgba(31, 51, 86, 0.06)`,
         [theme.breakpoints.down('sm')]: {
             borderRadius: 0,
         },
     })
 )
-    
+
 export const Card = ({ size = 'normal', ...rest }: CardProps) => {
     return <Root size={size} {...rest} />
 }
