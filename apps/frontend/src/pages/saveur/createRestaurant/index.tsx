7eec1fc23d25525e41c3965564cf6bcc3dfbc57c
Padoue <padoue@galadrim.fr>
2022-07-12 18:54:31 +0200

    creation resto via form done

--- apps/frontend/src/pages/saveur/createRestaurant/index.tsx
+++ apps/frontend/src/pages/saveur/createRestaurant/index.tsx
@@ -1,7 +1,10 @@
-import { CreateRestaurant } from 'apps/frontend/src/components/saveur/createRestaurant'
+import { CreateRestaurant } from '../../../components/saveur/createRestaurant'
 import MainLayout from '../../../components/layouts/MainLayout'
+import { useRights } from '../../../hooks/useRights'
+
 
 export const CreateRestaurantPage = () => {
+    useRights('all', ['USER_ADMIN'], '/saveur')
 
     return (
         <MainLayout fullscreen>
