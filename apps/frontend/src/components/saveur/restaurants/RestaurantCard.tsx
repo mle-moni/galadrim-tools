89b0b8f0d87665d817f9fdb39e5bb0e45316ac1c
Maxime Navoizat <maxime@galadrim.fr>
2022-07-29 11:21:16 +0200

    feat: restaurant ratings

--- apps/frontend/src/components/saveur/restaurants/RestaurantCard.tsx
+++ apps/frontend/src/components/saveur/restaurants/RestaurantCard.tsx
@@ -1,6 +1,7 @@
-import { IImage, IRestaurant } from '@galadrim-rooms/shared'
+import { IImage, IRestaurant, NOTES_VALUES } from '@galadrim-rooms/shared'
 import { Close, Edit, Room } from '@mui/icons-material'
 import FavoriteIcon from '@mui/icons-material/Favorite'
+import { Collapse } from '@mui/material'
 import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
@@ -8,10 +9,13 @@ import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 import { observer } from 'mobx-react-lite'
+import { useMemo } from 'react'
 import { getApiUrl } from '../../../api/fetch'
 import { MAX_ZOOM } from '../../../pages/saveur'
 import { CustomLink } from '../../Core/CustomLink'
+import { RestaurantCardStore } from '../RestaurantCardStore'
 import { SaveurStore } from '../SaveurStore'
+import RatingComponent from './RatingComponent'
 
 export const DEFAULT_RESTAURANT_IMAGE_PATH = '/default/restaurant.svg'
 
@@ -28,6 +32,8 @@ interface RestaurantCardProps {
 }
 
 export const RestaurantCard = observer<RestaurantCardProps>(({ restaurant, saveurStore }) => {
+    const store = useMemo(() => new RestaurantCardStore(restaurant), [restaurant])
+
     return (
         <Card
             key={restaurant.name}
@@ -43,9 +49,16 @@ export const RestaurantCard = observer<RestaurantCardProps>(({ restaurant, saveu
                     {restaurant.description}
                 </Typography>
             </CardContent>
+            <Collapse in={store.isRatingDevelopped}>
+                <RatingComponent
+                    onClick={(id) => {
+                        store.saveRating(id)
+                    }}
+                />
+            </Collapse>
             <CardActions sx={{ display: 'flex', justifyContent: 'space-around' }}>
-                <Button disabled size="small">
-                    <FavoriteIcon />
+                <Button onClick={() => store.toggleIsRatingDevelopped()} size="small">
+                    {store.rating ? NOTES_VALUES[store.rating] : <FavoriteIcon />}
                 </Button>
                 <CustomLink to={`/saveur/restaurants/${restaurant.id}`}>
                     <Edit />
