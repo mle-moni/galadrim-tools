d4c5e7cb5db2be4c4ed91257617393601b0a1732
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2022-08-20 13:28:29 +0200

    feat: possibility to create tag on restaurant creation/edit page

--- apps/frontend/src/pages/saveur/restaurants/createEdit/EditRestaurant.tsx
+++ apps/frontend/src/pages/saveur/restaurants/createEdit/EditRestaurant.tsx
@@ -1,10 +1,19 @@
 import { IRestaurant } from '@galadrim-rooms/shared'
-import { LocationOn, Message, Storefront, Style } from '@mui/icons-material'
+import { LocationOn, Message, Storefront, Style, Tag } from '@mui/icons-material'
 import BackIcon from '@mui/icons-material/ChevronLeft'
-import { Autocomplete, Button, InputAdornment, OutlinedInput, TextField } from '@mui/material'
+import {
+    Autocomplete,
+    Button,
+    InputAdornment,
+    Modal,
+    OutlinedInput,
+    Paper,
+    TextField,
+} from '@mui/material'
 import { observer } from 'mobx-react-lite'
 import { Fragment, useMemo } from 'react'
 import { getApiUrl } from '../../../../api/fetch'
+import { AppStore } from '../../../../globalStores/AppStore'
 import { SaveurStore } from '../../../../globalStores/SaveurStore'
 import { GaladrimLogo } from '../../../../reusableComponents/Branding/GaladrimLogo'
 import { CustomLink } from '../../../../reusableComponents/Core/CustomLink'
@@ -29,6 +38,8 @@ export const EditRestaurant = observer<EditRestaurantProps>((props) => {
             props.mode === 'edit' ? new RestaurantStore(props.restaurant) : new RestaurantStore(),
         []
     )
+
+    const tagsStore = AppStore.saveurStore.tagsStore
 
     return (
         <GaladrimRoomsCard size="large" sx={{ width: '100%', maxWidth: 600 }}>
@@ -85,6 +96,50 @@ export const EditRestaurant = observer<EditRestaurantProps>((props) => {
                     }
                     sx={{ mt: 2 }}
                 />
+                <Button
+                    sx={{ mt: 2 }}
+                    variant="contained"
+                    onClick={() => tagsStore.setCreationModalVisible(true)}
+                >
+                    Ajouter un tag
+                </Button>
+                <Modal
+                    open={tagsStore.creationModalVisible}
+                    onClose={() => tagsStore.setCreationModalVisible(false)}
+                    sx={{
+                        height: '100%',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Paper variant="outlined" sx={{ p: 6 }}>
+                        <OutlinedInput
+                            value={tagsStore.newTagName}
+                            onChange={(e) => {
+                                tagsStore.setNewTagName(e.target.value)
+                            }}
+                            fullWidth
+                            placeholder="Nom du tag"
+                            startAdornment={
+                                <InputAdornment position="start" sx={{ ml: 0.5, mr: 1 }}>
+                                    <Tag />
+                                </InputAdornment>
+                            }
+                            sx={{ mt: 2 }}
+                        />
+                        <Button
+                            onClick={() => tagsStore.createTag()}
+                            fullWidth
+                            disabled={!tagsStore.canSave}
+                            variant="contained"
+                            size="large"
+                            sx={{ mt: 2 }}
+                        >
+                            Ajouter le tag
+                        </Button>
+                    </Paper>
+                </Modal>
                 <Autocomplete
                     multiple
                     id="tags"
