816352bc4444004f1a2f39e79e988385ae11da7e
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2022-11-11 15:40:30 +0100

    chore: bumped version

--- apps/frontend/src/utils/patchNotesInfos.ts
+++ apps/frontend/src/utils/patchNotesInfos.ts
@@ -1,5 +1,9 @@
 // if you modify PATCH_VERSION, user will see PATCH_NOTES next time he goes on the galadrim-tools website
-export const PATCH_VERSION = '3.1.0'
+export const PATCH_VERSION = '3.2.0'
 
 // so, if you modify PATCH_VERSION, modify PATCH_NOTES too
-export const PATCH_NOTES = ['Nouveau système de patch notes']
+export const PATCH_NOTES = [
+    'Nouveau système de patch notes',
+    "L'utilisateur peut désormais modifier son profil",
+    'Les restaurants ont désormais un lien cliquable',
+]
