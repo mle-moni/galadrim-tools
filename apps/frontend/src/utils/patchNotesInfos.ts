39a87b31f265f19e2cbaf269009558b5bad2aace
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2022-11-11 12:51:52 +0100

    feat: bump version

--- apps/frontend/src/utils/patchNotesInfos.ts
+++ apps/frontend/src/utils/patchNotesInfos.ts
@@ -1,5 +1,5 @@
 // if you modify PATCH_VERSION, user will see PATCH_NOTES next time he goes on the galadrim-tools website
-export const PATCH_VERSION = '3.0.0'
+export const PATCH_VERSION = '3.1.0'
 
 // so, if you modify PATCH_VERSION, modify PATCH_NOTES too
 export const PATCH_NOTES = ['Nouveau système de patch notes']
