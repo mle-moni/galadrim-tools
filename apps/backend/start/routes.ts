2dd13cce5f7d5fe07fe60ba06b5d829d9e837cfb
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2022-11-23 13:37:53 +0100

    feat: load less events

--- apps/backend/start/routes.ts
+++ apps/backend/start/routes.ts
@@ -30,6 +30,7 @@ Route.post('/getOtp', 'AuthController.getOtp')
 
 Route.group(() => {
     Route.resource('events', 'EventsController').apiOnly()
+    Route.get('/allEvents', 'EventsController.all')
     Route.resource('tags', 'TagsController').apiOnly()
     Route.resource('restaurants', 'RestaurantsController').apiOnly()
     Route.resource('notes', 'RestaurantNotesController').apiOnly()
