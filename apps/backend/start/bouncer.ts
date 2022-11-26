90b1730e336badaff5db1a37f8ec2c8ab26468ba
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2022-11-26 16:55:06 +0100

    refactor: replaced App -> app

--- apps/backend/start/bouncer.ts
+++ apps/backend/start/bouncer.ts
@@ -55,6 +55,6 @@ export const { actions } = Bouncer
 |****************************************************************
 */
 export const { policies } = Bouncer.registerPolicies({
-    RestaurantsPolicy: () => import('../App/Policies/Restaurant'),
-    IdeasPolicy: () => import('../App/Policies/Idea'),
+    RestaurantsPolicy: () => import('../app/Policies/Restaurant'),
+    IdeasPolicy: () => import('../app/Policies/Idea'),
 })
