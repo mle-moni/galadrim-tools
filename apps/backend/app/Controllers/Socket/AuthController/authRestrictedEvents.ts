dcb8afc199808518c8ad8c32da1c39e2e3eba0a5
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2023-01-28 22:24:54 +0100

    feat: added livePos update for platformer game

--- apps/backend/app/Controllers/Socket/AuthController/authRestrictedEvents.ts
+++ apps/backend/app/Controllers/Socket/AuthController/authRestrictedEvents.ts
@@ -1,6 +1,7 @@
 import { IUserData, _assert } from '@galadrim-tools/shared'
 import { Socket } from 'socket.io'
 import { gameChat } from './tournois/gameChat'
+import { livePlayers, livePos } from './tournois/livePos'
 import { scoreTournois } from './tournois/scoreTournois'
 
 export const getSocketUser = (socket: Socket) => {
@@ -15,10 +16,20 @@ export const joinAuthRestrictedEvents = (socket: Socket) => {
 
     socket.on('gameChat', (data) => gameChat(socket, data))
     socket.on('scoreTournois', (data, mapId) => scoreTournois(socket, data, mapId))
+    socket.on('livePos', (data, password) => livePos(socket, data, password))
+    socket.on('disconnect', () => {
+        const user = getSocketUser(socket)
+        if (livePlayers.hasOwnProperty(user.username)) {
+            delete livePlayers[user.username]
+            socket.broadcast.emit('deletePlayer', user.username)
+        }
+    })
 }
 
 export const partAuthRestrictedEvents = (socket: Socket) => {
     socket.removeAllListeners('debug')
     socket.removeAllListeners('gameChat')
     socket.removeAllListeners('scoreTournois')
+    socket.removeAllListeners('livePos')
+    socket.removeAllListeners('disconnect')
 }
