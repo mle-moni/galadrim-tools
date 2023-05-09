c21a7da1e799c373e4a6e8a4ecf4823f0189b02d
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2023-05-09 13:06:19 +0200

    fix: trying to fix tournois query

--- apps/backend/app/Controllers/Http/platformerResults/PlatformerResultsController.ts
+++ apps/backend/app/Controllers/Http/platformerResults/PlatformerResultsController.ts
@@ -6,14 +6,15 @@ export default class PlatformerResultsController {
     async index({}: HttpContextContract) {
         const results = await PlatformerResult.query()
             .select('id', 'map_id', 'user_id', 'score', 'jumps', 'time')
-            .from('platformer_results')
-            .whereIn(Database.raw('(map_id, user_id, score)'), (builder) => {
-                builder
-                    .select('map_id', 'user_id', Database.raw('MIN(score)'))
-                    .from('platformer_results')
-                    .groupBy('map_id', 'user_id')
+            .from('platformer_results as p1')
+            .whereNotExists((q) => {
+                q.select('*')
+                    .from('platformer_results as p2')
+                    .where('p1.id', '!=', Database.raw('p2.id'))
+                    .where('p1.map_id', Database.raw('p2.map_id'))
+                    .where('p1.user_id', Database.raw('p2.user_id'))
+                    .where('p2.score', '<', Database.raw('p1.score'))
             })
-            .groupBy('map_id', 'user_id')
 
         return results
     }
