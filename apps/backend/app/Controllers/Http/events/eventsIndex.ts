ba5c164e41407fd6b52bbfddbb6f2448d7e845d6
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2023-07-10 19:22:23 +0200

    fix: eventsIndex

--- apps/backend/app/Controllers/Http/events/eventsIndex.ts
+++ apps/backend/app/Controllers/Http/events/eventsIndex.ts
@@ -1,10 +1,12 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Event from 'App/Models/Event'
 
-const EVENTS_LIMIT = 200
-
 export const indexRoute = async (_params: HttpContextContract) => {
-    const events = Event.query().limit(EVENTS_LIMIT).orderBy('id', 'desc')
+    const events = Event.query()
+        .whereRaw(
+            'events.start > DATE_SUB(NOW(), INTERVAL 7 DAY) AND events.start < DATE_ADD(NOW(), INTERVAL 7 DAY)'
+        )
+        .orderBy('id', 'desc')
 
     return events
 }
