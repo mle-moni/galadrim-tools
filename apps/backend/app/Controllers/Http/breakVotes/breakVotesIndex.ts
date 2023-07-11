c405b5acee029c3d401654ba623686035b535f90
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2023-07-11 18:58:20 +0200

    feat: breakVotesIndex

--- apps/backend/app/Controllers/Http/breakVotes/breakVotesIndex.ts
+++ apps/backend/app/Controllers/Http/breakVotes/breakVotesIndex.ts
@@ -2,5 +2,7 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import BreakVote from 'App/Models/BreakVote'
 
 export const breakVotesIndex = async ({}: HttpContextContract) => {
-    return BreakVote.query().preload('activities')
+    return BreakVote.query()
+        .whereRaw(`DATE_FORMAT(break_votes.created_at, '%Y-%m-%d') = curdate()`)
+        .preload('activities')
 }
