41ace2bb028f65951298db97bee4114a400b8ed1
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2023-07-12 13:34:06 +0200

    feat: preload times for breakVotes index

--- apps/backend/app/Controllers/Http/breakVotes/breakVotesIndex.ts
+++ apps/backend/app/Controllers/Http/breakVotes/breakVotesIndex.ts
@@ -4,5 +4,8 @@ import BreakVote from 'App/Models/BreakVote'
 export const TODAY_BREAK_VOTE_FILTER = "DATE_FORMAT(break_votes.created_at, '%Y-%m-%d') = curdate()"
 
 export const breakVotesIndex = async ({}: HttpContextContract) => {
-    return BreakVote.query().whereRaw(TODAY_BREAK_VOTE_FILTER).preload('activities')
+    return BreakVote.query()
+        .whereRaw(TODAY_BREAK_VOTE_FILTER)
+        .preload('activities')
+        .preload('times')
 }
