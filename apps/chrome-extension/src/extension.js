d68be4da55e485a32eab6b2861cac3288d12e71d
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2023-10-05 14:18:29 +0200

    feat: chrome-extension

---
+++ apps/chrome-extension/src/extension.js
@@ -0,0 +1,210 @@
+const ROOMS = new Set([
+    'Salle Vador',
+    'Salle Adier',
+    'Salle Turing',
+    'Salle manguier massif',
+    'Salle du coffre',
+    'Cuisine',
+    'Phone box',
+])
+
+const REFRESH_INTERVAL = 1000
+
+const MONTH_NUMBERS = {
+    janvier: 1,
+    février: 2,
+    mars: 3,
+    avril: 4,
+    mai: 5,
+    juin: 6,
+    juillet: 7,
+    août: 8,
+    septembre: 9,
+    octobre: 10,
+    novembre: 11,
+    décembre: 12,
+    january: 1,
+    february: 2,
+    march: 3,
+    april: 4,
+    may: 5,
+    june: 6,
+    july: 7,
+    august: 8,
+    september: 9,
+    october: 10,
+    november: 11,
+    december: 12,
+}
+
+const getHoursMinutes = (str) => {
+    const [hoursStr, minutesAndAmPm] = str.split(':')
+    const amPm = minutesAndAmPm.slice(-2).toLowerCase()
+    const minutes = minutesAndAmPm.slice(0, 2)
+    const hours = +hoursStr % 12
+
+    if (amPm === 'pm') {
+        return { hours: hours + 12, minutes }
+    }
+
+    return { hours, minutes }
+}
+
+let apiToken = null
+
+async function getRooms(start, end) {
+    const res = await fetch(
+        `https://galadrim-tools-api.mle-moni.fr/availableRooms?start=${start}&end=${end}`,
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiToken}`,
+            },
+        }
+    )
+
+    return res.json()
+}
+
+setInterval(() => {
+    chrome.storage.sync.get('galadrimToolsApiToken', function (items) {
+        apiToken = items.galadrimToolsApiToken
+    })
+
+    if (!apiToken) return
+
+    const dates = getDates()
+
+    if (!dates) return
+
+    displayAvailableRooms(dates.startIso, dates.endIso)
+}, REFRESH_INTERVAL)
+
+function getDates() {
+    const startDate = document.querySelector('[data-key=startDate]')
+    const startTime = document.querySelector('[data-key=startTime]')
+    const endTime = document.querySelector('[data-key=endTime]')
+
+    if (!startDate || !startTime || !endTime) return null
+
+    // startDate looks like this: Mercredi, 8 novembre [2024]
+    // We want to extract the date from it
+    const startDateText = startDate.innerText.split(', ')[1] // 8 novembre
+    const spaceSplit = startDateText.split(' ')
+    const day = spaceSplit[0]
+    const monthText = spaceSplit[1]
+    const month = MONTH_NUMBERS[monthText.toLowerCase()]
+    const year = spaceSplit.length === 2 ? new Date().getFullYear() : spaceSplit[2]
+
+    const { hours: startHours, minutes: startMinutes } = getHoursMinutes(startTime.innerText)
+    const { hours: endHours, minutes: endMinutes } = getHoursMinutes(endTime.innerText)
+
+    const monthStr = month.toString().padStart(2, '0')
+    const dayStr = day.toString().padStart(2, '0')
+    const startHoursStr = startHours.toString().padStart(2, '0')
+    const startMinutesStr = startMinutes.toString().padStart(2, '0')
+    const endHoursStr = endHours.toString().padStart(2, '0')
+    const endMinutesStr = endMinutes.toString().padStart(2, '0')
+
+    const parsedStartDate = `${year}-${monthStr}-${dayStr}T${startHoursStr}:${startMinutesStr}`
+    const parsedEndDate = `${year}-${monthStr}-${dayStr}T${endHoursStr}:${endMinutesStr}`
+
+    const startIso = new Date(parsedStartDate).toISOString()
+    const endIso = new Date(parsedEndDate).toISOString()
+
+    return { startIso, endIso }
+}
+
+function getSubmitButton() {
+    const spans = document.querySelector('div[role=dialog]').querySelectorAll('button > span')
+
+    return spans[spans.length - 1]
+}
+
+function getTitleValue() {
+    const titleInput = document.querySelector('div[role=dialog]').querySelector('input')
+    if (!titleInput) return null
+    return titleInput.value
+}
+
+async function displayAvailableRooms(start, end) {
+    const availableRooms = await getRooms(start, end)
+    const locationDiv = document.querySelector('[data-key=location]')
+
+    let select = document.getElementById('room-select')
+
+    if (!select) {
+        const listContainer = document.createElement('div')
+
+        listContainer.style.margin = '10px'
+        listContainer.style.marginLeft = '66px'
+
+        listContainer.id = 'room-select-container'
+        const parent =
+            locationDiv.parentElement.parentElement.parentElement.parentElement.parentElement
+                .parentElement.parentElement
+        parent.appendChild(listContainer)
+        const lab = document.createElement('label')
+        lab.htmlFor = 'room-select'
+        lab.innerText = 'Choisir une salle'
+
+        lab.style.marginRight = '10px'
+
+        select = document.createElement('select')
+        select.id = 'room-select'
+
+        listContainer.appendChild(lab)
+        listContainer.appendChild(select)
+
+        select.addEventListener('change', (e) => {
+            locationDiv.click()
+            setTimeout(() => {
+                parent.getElementsByTagName('input')[0].value = e.target.value
+            }, 100)
+        })
+
+        getSubmitButton().addEventListener('click', () => {
+            console.log('room chosen', select.value)
+            const isRoom = ROOMS.has(select.value)
+            console.log('isRoom', isRoom)
+            if (isRoom) {
+                const title = getTitleValue()
+                createEvent(start, end, select.value, title)
+            }
+        })
+    }
+
+    const prevValue = select.value
+
+    const options = availableRooms.map((room) => {
+        const opt = document.createElement('option')
+        opt.value = room
+        opt.innerText = room
+        return opt
+    })
+
+    select.innerHTML = `<option value="">--Choisir une salle--</option>`
+
+    options.forEach((opt) => select.appendChild(opt))
+
+    select.value = prevValue
+}
+
+async function createEvent(start, end, room, title) {
+    const formData = new FormData()
+    formData.append('start', start)
+    formData.append('end', end)
+    formData.append('room', room)
+    if (title) {
+        formData.append('title', title)
+    }
+
+    await fetch('https://galadrim-tools-api.mle-moni.fr/events', {
+        headers: {
+            Authorization: `Bearer ${apiToken}`,
+        },
+        method: 'POST',
+        body: formData,
+    })
+}
---
+++ apps/chrome-extension/src/popup.js
@@ -0,0 +1,15 @@
+function saveToken() {
+    const token = document.getElementById('api-token')?.value
+
+    if (!token) return
+
+    chrome.storage.sync.set({ galadrimToolsApiToken: token }, () => {
+        document.getElementById('token-infos').innerText = 'API token saved'
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('save-api-token')
+    if (!button) return
+    button.addEventListener('click', saveToken)
+})
