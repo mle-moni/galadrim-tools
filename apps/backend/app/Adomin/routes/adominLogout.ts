8eb4176002910c531082bd56afe7d36821da4889
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2024-07-13 12:50:22 +0200

    wip: upgrade-command-options

--- apps/backend/app/Adomin/routes/adominLogout.ts
+++
@@ -1,7 +0,0 @@
-import { HttpContext } from '@adonisjs/core/http'
-
-export const adominLogout = async ({ auth }: HttpContext) => {
-    await auth.use('api').logout()
-
-    return { message: 'Au revoir !' }
-}
--- apps/backend/app/Adomin/routes/validateResourceId.ts
+++
@@ -1,7 +0,0 @@
-import { schema, validator } from '@adonisjs/validator'
-
-const resourceIdSchema = schema.create({ id: schema.number() })
-
-export const validateResourceId = (data: unknown) => {
-    return validator.validate({ schema: resourceIdSchema, data })
-}
