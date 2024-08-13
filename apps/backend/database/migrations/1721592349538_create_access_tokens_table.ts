116e3739c8b5cfd49a2d64cbbbaeee818a80f54c
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2024-08-13 18:44:03 +0200

    fix: down migration

--- apps/backend/database/migrations/1721592349538_create_access_tokens_table.ts
+++ apps/backend/database/migrations/1721592349538_create_access_tokens_table.ts
@@ -22,5 +22,24 @@ export default class extends BaseSchema {
 
     async down() {
         this.schema.dropTable(this.tableName);
+
+        this.schema.createTable("api_tokens", (table) => {
+            table.increments("id").primary();
+            table
+                .integer("user_id")
+                .unsigned()
+                .references("id")
+                .inTable("users")
+                .onDelete("CASCADE");
+            table.string("name").notNullable();
+            table.string("type").notNullable();
+            table.string("token", 64).notNullable().unique();
+
+            /**
+             * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
+             */
+            table.timestamp("expires_at", { useTz: true }).nullable();
+            table.timestamp("created_at", { useTz: true }).notNullable();
+        });
     }
 }
