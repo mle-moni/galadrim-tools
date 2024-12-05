d78eecbce10a3c4f21153bcc79312a1b3694bdf0
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2024-12-05 18:33:14 +0100

    fix: sensor report :eyes:

--- apps/backend/app/controllers/sensors/handle_report.ts
+++ apps/backend/app/controllers/sensors/handle_report.ts
@@ -1,30 +1,31 @@
 import type { HttpContext } from "@adonisjs/core/http";
-import vine from "@vinejs/vine";
 import { DateTime } from "luxon";
 
-const validationSchema = vine.compile(
-    vine.object({
-        id: vine.any(),
-        temp: vine.any(),
-        lux: vine.any(),
-        motion: vine.any(),
-        tamper: vine.any(),
-        bat: vine.any(),
-    }),
-);
+// const validationSchema = vine.compile(
+//     vine.object({
+//         id: vine.any(),
+//         temp: vine.any(),
+//         lux: vine.any(),
+//         motion: vine.any(),
+//         tamper: vine.any(),
+//         bat: vine.any(),
+//     }),
+// );
 
 export const handleReport = async ({ request }: HttpContext) => {
     console.log("--- START REPORT ---");
     console.log(DateTime.now().toFormat("yyyy-MM-dd HH:mm:ss"));
 
-    const { id, bat, tamper, motion, lux, temp } = await request.validateUsing(validationSchema);
+    console.log(JSON.stringify(request.all()));
 
-    console.log("id", id);
-    console.log("bat", bat);
-    console.log("tamper", tamper);
-    console.log("motion", motion);
-    console.log("lux", lux);
-    console.log("temp", temp);
+    // const { id, bat, tamper, motion, lux, temp } = await request.validateUsing(validationSchema);
+
+    // console.log("id", id);
+    // console.log("bat", bat);
+    // console.log("tamper", tamper);
+    // console.log("motion", motion);
+    // console.log("lux", lux);
+    // console.log("temp", temp);
     console.log("--- END REPORT ---");
 
     return { message: "OK" };
