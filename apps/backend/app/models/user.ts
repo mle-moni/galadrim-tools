d76ba75d7c732a1fa80f8d17cee3d10899dd27f5
LE MONIES DE SAGAZAN Mayeul <m.lemoniesdesagazan@gmail.com>
2024-12-17 16:52:16 +0100

    fix: security

--- apps/backend/app/models/user.ts
+++ apps/backend/app/models/user.ts
@@ -47,7 +47,7 @@ export default class User extends compose(BaseModel, AuthFinder) {
     @column()
     declare imageUrl: string;
 
-    @column()
+    @column({ serialize: () => null })
     declare otpToken: string | null;
 
     @column()
