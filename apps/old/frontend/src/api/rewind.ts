39928c6e0f80cd02cb7eb9dea601f263c0b24bee
ggmolly <molly@molly.sh>
2025-12-27 02:31:12 +0100

    chore: ggwp

--- apps/old/frontend/src/api/rewind.ts
+++
@@ -1,7 +0,0 @@
-import { fetchBackend } from "./fetch";
-
-export const fetchRewindInfos = async () => {
-    const res = await fetchBackend("/rewind");
-    if (!res.ok) return [];
-    return res.json();
-};
--- apps/old/frontend/src/pages/saveur/restaurants/ratingsFunctions.ts
+++
@@ -1,9 +0,0 @@
-import type { UserData } from "../../../api/galadrimeurs";
-
-export const getNameOfUsers = (userIds: number[], usersMap: Map<number, UserData>) => {
-    const users = userIds
-        .map((id) => usersMap.get(id)?.username)
-        .filter((username) => username !== undefined);
-
-    return users.join(", ");
-};
